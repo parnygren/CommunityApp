c58e1eac2a67165c582a3290edb13c4dd903d9a0
Pär Nygren <par.a.nygren@gmail.com>
2017-03-26 07:33:49 +0200

    Added a data cache to event and events modules

--- src/event.js
+++ src/event.js
@@ -1,4 +1,12 @@
+import {inject} from 'aurelia-framework';
+import {DataCache} from 'dataCache';
+
+@inject(DataCache)
 export class Event {
+    constructor(cache) {
+        cache.data.push('b');
+    }
+    
     activate(bindingContext) {
         this.item = bindingContext;
     }
--- src/events.js
+++ src/events.js
@@ -1,8 +1,14 @@
+import {inject} from 'aurelia-framework';
+import {DataCache} from 'dataCache';
+
+@inject(DataCache)
 export class Events {
-    constructor() {
+    constructor(dataCache) {
         this.events = [
             {id: 1, title: "Aurelia Fundamentals"},
             {id: 2, title: "Data-Centric SPAs with BreezeJS"}
         ];
+        this.cache = dataCache;
+        this.cache.data.push('a');
     }
 }
